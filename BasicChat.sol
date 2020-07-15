1fe589ef22a214cbd0b106964b7e10449d62a137
Tim Beutelspacher <46922380+TimBeutelspacher@users.noreply.github.com>
2020-07-15 12:01:57 +0200

    Update BasicChat.sol

--- BasicChat.sol
+++ BasicChat.sol
@@ -1,20 +1,38 @@
+/*
+    2
+*/
+
+// in diesem Contract wird die selbe Version von Solidity genutzt wie bei der Message
 pragma solidity ^0.6.6;
 
+// Importieren die Message, da ein Chat mehrere Message-Objekte beinhalten soll
 import "./BasicMessage.sol";
 
+// Wie eine Klasse wird der Chat als Contract gekennzeichnet
 contract Chat {
     
-    address private latestMessage = 0x0000000000000000000000000000000000000000;
+    // speichern der adresse der zuletzt geschriebenen Message
+    // initialwert ist 0x0000000000000000000000000000000000000000
+    address private latestMessage;
     
-    //Funktion um eine Nachricht in den Chat zu schreiben
+    // Funktion um eine Nachricht in den Chat zu schreiben
+    // Übergeben der Textnachricht als String
     function createMessage(string memory _message) public{
+        
+        // Instanziieren des Message-contracts mit den angegebenen Übergabe parametern
         Message message = new Message(_message, msg.sender, latestMessage);
+        
+        // setzen der Adresse des neuen Contracts
         latestMessage = address(message);
     }
     
-    //Funktion um sich die letzte Nachricht eines Chats ausgeben zu lassen.
+    //Funktion um sich die adresse der letzten Nachricht des Chats ausgeben zu lassen.
     function getLatestMessage() public view returns(address){
+        
+        // falls das Attribut latestMessage noch den default Wert einer Adresse hat, wurde noch keine Nachricht geschrieben. 
         require (latestMessage != 0x0000000000000000000000000000000000000000, "There is no message in this chat.");
+        
+        // Ausgeben der adresse, an welcher der Contract der letzten verfassten Nachricht liegt.
         return latestMessage;
     }
 }
