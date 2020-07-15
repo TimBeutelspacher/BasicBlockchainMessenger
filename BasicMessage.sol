9b4f7e551aa3807675d3cb297a14660388a5f978
Tim Beutelspacher <46922380+TimBeutelspacher@users.noreply.github.com>
2020-07-15 11:59:39 +0200

    Update BasicMessage.sol

--- BasicMessage.sol
+++ BasicMessage.sol
@@ -1,14 +1,31 @@
+/*
+    1
+*/
+
+// Version von solidity bei 0.6.6 werden alle benötigten Funktionen unterstützt
 pragma solidity ^0.6.6;
 
+// Contract Message kennzeichnen
 contract Message {
     
     //Variablen
+    
+    // Textnachricht speichern
     string public text;
+    
+    // Adresse des authors speichern
     address public author;
+    
+    // Referenz auf die Adresse der vorangegangenen Nachricht
     address public previousMessage;
     
-    //Konstruktor
+    // Konstruktor wird beim iniziieren des Contracts aufgerufen
+    // Text wird gesetzt. memory weil die Variable nur bis zum ende der Funktion gespiechert werden soll.
+    // Author wird gesetzt
+    // previousMessage wird gesetzt
     constructor(string memory _text, address _author, address _previousMessage) public{
+        
+        // Übergebene Parameter als Attribute setzen
         text = _text;
         author = _author;
         previousMessage = _previousMessage;
